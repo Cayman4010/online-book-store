14024595d1f4d500c19bf644fd2c9b1c2f67e549
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-11-01 23:35:11 +0200

    created JwtAuthenticationFilter and fixed mistakes

--- src/main/java/bookstore/security/JwtUtil.java
+++ src/main/java/bookstore/security/JwtUtil.java
@@ -1,22 +1,38 @@
 package bookstore.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.Date;
-
 @Component
 public class JwtUtil {
-    private Key secret;
+    private SecretKey secret;
     @Value("${jwt.expiration}")
     private Long expiration;
 
     public JwtUtil(@Value("${jwt.secret}") String secret) {
         this.secret = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String getUsername(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser()
+                .verifyWith(secret)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claimsResolver.apply(claims);
     }
 
     public String generateToken(String userName) {
@@ -26,5 +42,17 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(secret)
                 .compact();
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(secret)
+                    .build()
+                    .parseSignedClaims(token);
+            return !claimsJws.getPayload().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException ex) {
+            throw new JwtException("Token is expired or invalid");
+        }
     }
 }
