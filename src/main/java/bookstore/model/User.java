47d9097c0da49f993e10ba218b06968b1ea51471
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-11-07 10:42:32 +0200

    Changed @RequiredArgsConstructor to @NoArgsConstructor in User entity

--- src/main/java/bookstore/model/User.java
+++ src/main/java/bookstore/model/User.java
@@ -14,7 +14,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -22,7 +22,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 @SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
 @Where(clause = "is_deleted=false")
